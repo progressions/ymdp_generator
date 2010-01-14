63b567c8cab7b6b129c7d5542fa23ceff5615ce7
Jeff Coleman <progressions@gmail.com>
2010-01-14 13:28:28 -0600

    yay

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,27 @@ require 'spec/autorun'
 Spec::Runner.configure do |config|
   
 end
+
+
+
+def stub_screen_io
+  # stub screen I/O
+  $stdout.stub!(:puts)
+  $stdout.stub!(:print)    
+end
+
+def stub_file_io(unprocessed_file)
+  # stub file I/O
+  @file ||= mock('file').as_null_object
+  @file.stub!(:read).and_return(unprocessed_file)
+    
+  File.stub!(:exists?).and_return(false)
+  File.stub!(:open).and_yield(@file) 
+end
+
+def stub_erb(processed_file)
+  # stub ERB
+  @erb ||= mock('erb').as_null_object
+  @erb.stub!(:result).and_return(processed_file)
+  ERB.stub!(:new).and_return(@erb)
+end
--- spec/ymdp_generator_spec.rb
+++ spec/ymdp_generator_spec.rb
@@ -2,9 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Generate" do
   before(:each) do
-    # stub screen I/O
-    $stdout.stub!(:puts)
-    $stdout.stub!(:print)
+    stub_screen_io
     
     @template_path = "templates"
     @application_path = "app"
@@ -64,22 +62,6 @@ describe "Generate" do
         @base.generate("funk")
       end
     end
-  end
-  
-  def stub_file_io(unprocessed_file)
-    # stub file I/O
-    @file ||= mock('file').as_null_object
-    @file.stub!(:read).and_return(unprocessed_file)
-      
-    File.stub!(:exists?).and_return(false)
-    File.stub!(:open).and_yield(@file) 
-  end
-  
-  def stub_erb(processed_file)
-    # stub ERB
-    @erb ||= mock('erb').as_null_object
-    @erb.stub!(:result).and_return(processed_file)
-    ERB.stub!(:new).and_return(@erb)
   end
   
   describe "subclasses" do
